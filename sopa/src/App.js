3c20304918e772865d2be761e4fee2126a10d44c
Diogo <diogofgf97@gmail.com>
2022-06-05 17:08:40 +0100

    Header Iniciado

--- sopa/src/App.js
+++ sopa/src/App.js
@@ -2,27 +2,30 @@ import logo from './logo.svg';
 //import './assets/css/app.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Ola from './components/Ola';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    /* 
+    == Components ==
+    - Header -
+      > Título
+    - Menu inicial -
+      > começar jogo
+      > ver highscores
+    - Tabuleiro de jogo -
+    - Lista de palavras -
+    - Pontuação final -
+    - Lista de pontuações -
+    - tempo de jogo -
+    - seleção de nível - 
+    - pop-up fim do jogo -
+    - Footer -
+      > "trabalho realizado por X, Y e Z no âmbito da cadeira de Linguagens Script"
+    */
     <div className="App">
     <Header />
+    <Ola />
     <Footer />
     </div>
   );
---
+++ sopa/src/components/Ola.js
@@ -0,0 +1,11 @@
+import '../assets/css/ola.css'
+
+function Ola() {
+    
+    return( 
+        <div> ola </div>
+    );
+}
+
+
+export default Ola;
