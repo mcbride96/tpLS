1c6c6dd091ad3844e1ecf66522771921d446bf9f
mcbride96 <mcbride9696@gmail.com>
2022-06-07 01:26:02 +0100

    footerUpdated

--- sopa/src/components/Footer.js
+++ sopa/src/components/Footer.js
@@ -4,13 +4,10 @@ function Footer() {
     return (
         <footer>
             <h3 id="realizado">Este trabalho foi realizado no âmbito da disciplina de Linguagens Script, por:</h3> 
-            <div>Diogo Franco</div>
-            <div>Filipe Carvalho</div>
-            <div>Lourenço McBride</div>
+            <div>Diogo Franco  *  Filipe Carvalho  *  Lourenço McBride</div>
             <br></br>
             <div>ISEC 2021/22</div>
             <img src="logo.png"></img> 
-            
         </footer>
     );
 }
